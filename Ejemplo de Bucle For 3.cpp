549b54d37e1b1b58fcaf3c9d078d44d43cad5669
Maria Castro <92470095+Mariacastro28@users.noreply.github.com>
2021-11-21 00:44:35 -0400

    Add files via upload

---
+++ Ejemplo de Bucle For 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int n, suma = 0, nota1, nota2;
+	double promedio;
+	
+for( int i =0 ; i < 2; i++){
+	 cout<<"Estudiante "<<i+1<<endl;
+	 
+	 cout<<"Nota 1: ";
+	 cin >> nota1;
+	 
+	 cout<<"Nota 2: ";
+	 cin >> nota2;
+}
+
+suma = nota1 + nota2;
+promedio = suma / 2.0;
+
+      cout<<"\nPromedio es: "<<promedio<<"\n\n";
+	 	return 0;
+}
